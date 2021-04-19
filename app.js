b8dc0edbc16853030f28ff6fac91ede242e14d53
mansi <mansisinghal1181@gmail.com>
2021-04-20 01:38:44 +0530

    Updated Website

--- app.js
+++ app.js
@@ -45,12 +45,16 @@ app.post("/", function(req,res){
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
-})
+});
 
 app.post("/work", function(req,res){
   let item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
+});
+
+app.get("/about", function(req,res){
+  res.render("about");
 });
 
 app.listen(3000, function(){

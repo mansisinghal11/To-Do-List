e8caa1e7b4bc1e3f8805eae03f6f8f31e559d185
mansi <mansisinghal1181@gmail.com>
2021-04-24 01:21:01 +0530

    Added database

--- app.js
+++ app.js
@@ -2,49 +2,140 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
-
-let items = [];
-let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item ({
+  name: "Click the + button to add a newitem."
+});
+
+const item3 = new Item ({
+  name: "<-- Click this to delete an item. "
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+let today = new Date();
+
+let options = {
+  weekday: "long",
+  day: "numeric",
+  month:"long"
+};
+
+let day = today.toLocaleDateString("en-US", options);
+
 app.get("/", function(req,res){
 
-  let today = new Date();
+  Item.find({}, function(err, foundItems){
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month:"long"
-  };
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
+        if (err){
+          console.log(err);
+        } else {
+          console.log("Succesfully inserted items");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {listTitle: day, newListItems: foundItems});
+    }
+  });
 
-  let day = today.toLocaleDateString("en-US", options);
+});
 
-  res.render("list", {listTitle: day, newListItems: items});
+app.get("/about", function(req,res){
+  res.render("about");
+});
 
+app.get("/:customListName", function(req,res){
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if(!err){
+      if(!foundList){
+        //Create a new list
+        const list = new List ({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName)
+      } else {
+        // show an existing list\
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
 });
 
 app.post("/", function(req,res){
 
-  let item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else{
-    items.push(item);
+  const item = new Item ({
+    name: itemName
+  });
+
+  if (listName === day){
+    item.save();
     res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName)
+    });
   }
-
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post("/delete", function(req,res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === day){
+    Item.findByIdAndRemove( checkedItemId,function(err){
+      if(!err) {
+        console.log("Succesfully deleted checked item");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate ({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if(!err) {
+        res.redirect("/" + listName);
+      }
+    });
+  }
+
 });
 
 app.post("/work", function(req,res){
@@ -53,9 +144,7 @@ app.post("/work", function(req,res){
   res.redirect("/work");
 });
 
-app.get("/about", function(req,res){
-  res.render("about");
-});
+
 
 app.listen(process.env.PORT ||3000, function(){
   console.log("Server is running on port 3000");
